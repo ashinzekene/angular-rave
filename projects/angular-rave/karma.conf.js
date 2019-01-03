9a54f7abf531d96ddeae01fa8e326246da378cbd
ashinzekene <ashinzekene@gmail.com>
2019-01-03 10:15:07 +0100

    travis should work now

--- projects/angular-rave/karma.conf.js
+++ projects/angular-rave/karma.conf.js
@@ -24,8 +24,14 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
+    autoWatch: false,
     browsers: ['Chrome'],
-    singleRun: false
+    singleRun: false,
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    }
   });
 };
